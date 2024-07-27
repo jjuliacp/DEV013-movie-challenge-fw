83d187a5cccb519572aed4b0077f05b90019b964
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2024-07-27 13:49:10 -0500

    definir type de movie #8

---
+++ src/models/movie.d.ts
@@ -0,0 +1,10 @@
+type Movie = {
+    id: number;
+    title: string;
+    overview: string;
+    poster: string;
+    year: number;
+    genre: string[];
+    ranking: number;
+}
+export default Movie
