e0e60ec4ea25346bac6e125c7f2c9391cb71cd96
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2024-08-23 10:38:36 -0500

    feat: agregar funcion getmovieDetail #26

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,4 +1,6 @@
+import Movie from "../models/movie";
 import { TOKEN_API } from "../utils/config";
+import { formatGenresToMap, formatMovie } from "../utils/transformers";
 
 export async function getMovieGenres(): Promise<[{ id: number; name: string }]> {
     const urlGenres = 'https://api.themoviedb.org/3/genre/movie/list'
@@ -19,6 +21,31 @@ export async function getMovieGenres(): Promise<[{ id: number; name: string }]> 
         }));
     } catch (error) {
         console.error(`Error fetching movie genres: ${error}`);
+        throw error;
+    }
+}
+
+export async function getMovieDetail(movie_id: number): Promise<Movie> {
+    const urlMovie = `https://api.themoviedb.org/3/movie/${movie_id}`
+    try {
+        const responseMovie = await fetch(urlMovie, {
+            headers: {
+                authorization: `Bearer ${TOKEN_API}`,
+            },
+        });
+        if (!responseMovie.ok) {
+            throw new Error(`Failed to fetch movie details: ${responseMovie.statusText}`);
+        }
+        const movieData = await responseMovie.json();
+        const genres = await getMovieGenres() // obtengo generos, convertir los generos
+        const genresNames = formatGenresToMap(genres)
+        // Transformar los datos de la API al modelo de negocio Movie
+        const movie: Movie = formatMovie(movieData, genresNames);
+
+        return movie;
+
+    } catch (error) {
+        console.error(`Error fetching movie details: ${error}`);
         throw error;
     }
 }
