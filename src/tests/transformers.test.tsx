f39c6f8172b250ea9edb50c61db987e56a3a1402
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2024-09-06 11:47:56 -0500

    test: arreglar el test de transformers de acuerdo al formatMovie

--- src/tests/transformers.test.tsx
+++ src/tests/transformers.test.tsx
@@ -1,6 +1,21 @@
-/*import { formatMovie } from "../utils/transformers";
+import {
+  formatGenresToMap,
+  formatGenresToOptions,
+  formatMovie,
+} from "../utils/transformers";
 import { expectedMovieEmpty, movieDataEmpty } from "./mockdata";
-
+const genres = [
+  { id: 28, name: "Action" },
+  { id: 35, name: "Comedy" },
+];
+const genresMap = new Map<number, string>([
+  [28, "Action"],
+  [35, "Comedy"],
+]);
+const expectedOptions = [
+  { value: "28", label: "Action" },
+  { value: "35", label: "Comedy" },
+];
 describe("Transformers", () => {
   test("Transforms object into a type Movie object", () => {
     const movie = {
@@ -26,10 +41,25 @@ describe("Transformers", () => {
       voteCount: 0,
     };
 
-    expect(formatMovie(movie)).toEqual(expectedMovie);
+    expect(formatMovie(movie, genresMap)).toEqual(expectedMovie);
   });
+
   test("Handles empty string values correctly", () => {
-    expect(formatMovie(movieDataEmpty)).toEqual(expectedMovieEmpty);
+    expect(formatMovie(movieDataEmpty, genresMap)).toEqual(expectedMovieEmpty);
+  });
+  test("Convert array of genders to map ", () => {
+    expect(formatGenresToMap(genres)).toEqual(genresMap);
+  });
+  test("return map empty if the array of genres is empty", () => {
+    const genres: { id: number; name: string }[] = [];
+    const expectedMap = new Map<number, string>();
+    expect(formatGenresToMap(genres)).toEqual(expectedMap);
+  });
+  test("convert array of genres into array with options", () => {
+    const genresOption = [
+      { id: "28", name: "Action" },
+      { id: "35", name: "Comedy" },
+    ];
+    expect(formatGenresToOptions(genresOption)).toEqual(expectedOptions);
   });
 });
-*/
