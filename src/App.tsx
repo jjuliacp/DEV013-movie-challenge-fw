843e0b0238212247b86435201945e7ea806882d5
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2025-07-26 14:39:17 -0500

    fix: import moviedetail

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
 
-// import Start from "./Start";
-
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Start />} /> */}
       <Route path="/" element={<Home />} />
       <Route path="/movie/:id" element={<MovieDetail />} />
     </Routes>
